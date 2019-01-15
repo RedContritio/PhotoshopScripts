a860f244d7bcd1982e59d2ad93207e2796e18f8a
RedContritio <591369735@qq.com>
2019-01-16 01:40:01 +0800

    Updating

--- 003 FindEdges/main.jsx
+++ 003 FindEdges/main.jsx
@@ -1,4 +1,5 @@
-﻿var samplesFolder = Folder("C:\\Users\\RedContritio\\Documents\\Adobe Scripts\\PhotoshopScripts\\003 Decoloration\\img");
+﻿var BasicPath = "C:\\Users\\RedContritio\\Documents\\Adobe Scripts\\PhotoshopScripts\\003 FindEdges\\";
+var samplesFolder = Folder(BasicPath+"img");
 
 var fileList = samplesFolder.getFiles ();
 
@@ -12,9 +13,50 @@ for( var i=0; i<fileList.length; i++)
 		var idFndE = charIDToTypeID( "FndE" );
 		executeAction( idFndE, undefined, DialogModes.NO );
 		
-		break;
-		
-		app.activeDocument.close();
-		break;
+		SaveAsPngWithName(BasicPath+"dst\\"+i+".png");
+//		app.activeDocument.close ();
+		CloseNowDocumentWithoutSaving();
 	}
 }
+
+function SaveAsPngWithName( name)
+{
+	var idsave = charIDToTypeID( "save" );
+	var desc0 = new ActionDescriptor();
+	var idAs = charIDToTypeID( "As  " );
+	var desc1 = new ActionDescriptor();
+	var idPGIT = charIDToTypeID( "PGIT" );
+	var idPGIT = charIDToTypeID( "PGIT" );
+	var idPGIN = charIDToTypeID( "PGIN" );
+	desc1.putEnumerated( idPGIT, idPGIT, idPGIN );
+	var idPNGf = charIDToTypeID( "PNGf" );
+	var idPNGf = charIDToTypeID( "PNGf" );
+	var idPGAd = charIDToTypeID( "PGAd" );
+	desc1.putEnumerated( idPNGf, idPNGf, idPGAd );
+	var idCmpr = charIDToTypeID( "Cmpr" );
+	desc1.putInteger( idCmpr, 9 );
+	var idPNGF = charIDToTypeID( "PNGF" );
+	desc0.putObject( idAs, idPNGF, desc1 );
+	var idIn = charIDToTypeID( "In  " );
+	desc0.putPath( idIn, new File( name) );
+	var idDocI = charIDToTypeID( "DocI" );
+	desc0.putInteger( idDocI, 140 );
+	var idCpy = charIDToTypeID( "Cpy " );
+	desc0.putBoolean( idCpy, true );
+	var idsaveStage = stringIDToTypeID( "saveStage" );
+	var idsaveStageType = stringIDToTypeID( "saveStageType" );
+	var idsaveSucceeded = stringIDToTypeID( "saveSucceeded" );
+	desc0.putEnumerated( idsaveStage, idsaveStageType, idsaveSucceeded );
+	executeAction( idsave, desc0, DialogModes.NO );
+}
+
+function CloseNowDocumentWithoutSaving()
+{
+	var idCls = charIDToTypeID( "Cls " );
+	var desc = new ActionDescriptor();
+	var idSvng = charIDToTypeID( "Svng" );
+	var idYsN = charIDToTypeID( "YsN " );
+	var idN = charIDToTypeID( "N   " );
+	desc.putEnumerated( idSvng, idYsN, idN );
+	executeAction( idCls, desc, DialogModes.NO );
+};
---
+++ 004 Gradient/main.jsx
@@ -0,0 +1,89 @@
+﻿function color2desc( R, G, B)
+{
+	var desc = new ActionDescriptor();
+	desc.putDouble( charIDToTypeID( "Rd  "), R);
+	desc.putDouble( charIDToTypeID( "Grn "), G);
+	desc.putDouble( charIDToTypeID( "Bl  "), B);
+	return desc;
+}
+
+function colorLabel2desc( rgbdesc, pos)
+{
+	var desc = new ActionDescriptor();
+	desc.putObject( charIDToTypeID( "Clr "), charIDToTypeID( "RGBC"), rgbdesc);
+	desc.putEnumerated( charIDToTypeID( "Type"), charIDToTypeID( "Clry"), charIDToTypeID( "UsrS"));
+	desc.putInteger( charIDToTypeID( "Lctn"), pos*4096);
+	desc.putInteger( charIDToTypeID( "Mdpn"), 50);
+	return desc;
+}
+function colorLables2List( labels)
+{
+	var list = new ActionList();
+	for( var i = 0; i < labels.length; i++)
+	{
+		list.putObject( charIDToTypeID( "Clrt"), labels[i]);
+	}
+	return list;
+}
+
+function transparent2desc( pos, alpha)
+{
+	var desc = new ActionDescriptor();
+	desc.putUnitDouble( charIDToTypeID( "Opct"), charIDToTypeID( "#Prc"), alpha*100);
+	desc.putInteger( charIDToTypeID( "Lctn"), pos*4096);
+	desc.putInteger( charIDToTypeID( "Mdpn"), 50 );
+	return desc;
+}
+function transparentLables2List( labels)
+{
+	var list = new ActionList();
+	for( var i = 0; i < labels.length; i++)
+	{
+		list.putObject( charIDToTypeID( "Trns"), labels[i]);
+	}
+	return list;
+}
+var idMk = charIDToTypeID( "Mk  " );
+    var desc146 = new ActionDescriptor();
+    var idnull = charIDToTypeID( "null" );
+        var ref27 = new ActionReference();
+        var idGrdn = charIDToTypeID( "Grdn" );
+        ref27.putClass( idGrdn );
+    desc146.putReference( idnull, ref27 );
+    var idUsng = charIDToTypeID( "Usng" );
+        var desc147 = new ActionDescriptor();
+        var idGrad = charIDToTypeID( "Grad" );
+            var desc148 = new ActionDescriptor();
+            var idNm = charIDToTypeID( "Nm  " );
+            desc148.putString( idNm, """渐变名称""" );
+            var idGrdF = charIDToTypeID( "GrdF" );
+            var idGrdF = charIDToTypeID( "GrdF" );
+            var idCstS = charIDToTypeID( "CstS" );
+            desc148.putEnumerated( idGrdF, idGrdF, idCstS );
+            var idIntr = charIDToTypeID( "Intr" );
+            desc148.putDouble( idIntr, 4096.000000 );
+            var idClrs = charIDToTypeID( "Clrs" );
+					
+                    var desc150 = color2desc( 255.000000, 0.000000, 0.000000);
+                    var desc149 = colorLabel2desc( desc150, 0);
+                    var desc152 = color2desc( 0.000000, 0.000000, 120.000000 );
+				  var desc151 = colorLabel2desc ( desc152, 1);
+				  var colorlabels = new Array();
+				  colorlabels[0] = desc149;
+				  colorlabels[1] = desc151;
+					
+                var list22 = colorLables2List( colorlabels);
+            desc148.putList( idClrs, list22 );
+			
+			var idTrns = charIDToTypeID( "Trns" );
+			var desc153 = transparent2desc( 0, 1);
+			var desc154 = transparent2desc( 1, 1);
+			var transparentlabels = new Array( desc153, desc154);
+			var list23 = transparentLables2List( transparentlabels);
+			
+            desc148.putList( idTrns, list23 );
+        var idGrdn = charIDToTypeID( "Grdn" );
+        desc147.putObject( idGrad, idGrdn, desc148 );
+    var idGrdn = charIDToTypeID( "Grdn" );
+    desc146.putObject( idUsng, idGrdn, desc147 );
+executeAction( idMk, desc146, DialogModes.NO );
