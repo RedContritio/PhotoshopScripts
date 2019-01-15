445dc12bd5f240f5f94b48c146d25843f060547f
RedContritio <591369735@qq.com>
2019-01-16 00:07:59 +0800

    Update

---
+++ 003 Decoloration/main.jsx
@@ -0,0 +1,20 @@
+﻿var samplesFolder = Folder("C:\\Users\\RedContritio\\Documents\\Adobe Scripts\\PhotoshopScripts\\003 Decoloration\\img");
+
+var fileList = samplesFolder.getFiles ();
+
+for( var i=0; i<fileList.length; i++)
+{
+	if( fileList[i] instanceof File)
+	{
+		open( fileList[i]);
+		var layer = app.activeDocument.activeLayer;
+//		layer.applyStyle ( stringIDToTypeID("FndE"));
+		var idFndE = charIDToTypeID( "FndE" );
+		executeAction( idFndE, undefined, DialogModes.NO );
+		
+		break;
+		
+		app.activeDocument.close();
+		break;
+	}
+}
