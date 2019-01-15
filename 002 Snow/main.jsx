cbf6f50b1e11aec6d0a191ecfdc4ad92a9c5e3a0
RedContritio <591369735@qq.com>
2019-01-15 22:24:09 +0800

    Update

--- 002 Snow/main.jsx
+++ 002 Snow/main.jsx
@@ -6,10 +6,20 @@ while( isNaN ( stemsAmount) || stemsAmount <= 0 || stemsAmount > 100)
     alert("Please enter a number in range from 2 to 100");
     stemsAmount = prompt("How many stems do you need?", 6, "Processing \""+originalStem.name+"\""); 
 }
-var angle = 360 / stemsAmount;
-for(var i = 1; i < stemsAmount; i++)
+
+if( stemsAmount != null)
 {
-    var newStem = originalStem.duplicate ();
-    newStem.name = originalStem.name + " " + i;
-    newStem.rotate( angle*i, AnchorPosition.BOTTOMCENTER);
+    var angle = 360 / stemsAmount;
+    var stemsGroup = app.activeDocument.layerSets.add();
+    stemsGroup.name = originalStem.name + "(" + stemsAmount + " stems)";
+//    originalStem.move( stemsGroup, ElementPlacement.INSIDE);
+    for(var i = 0; i < stemsAmount; i++)
+    {
+        var newStem = originalStem.duplicate ();
+        newStem.rotate( angle*i, AnchorPosition.BOTTOMCENTER);
+        newStem.name = originalStem.name + " " + i;
+        newStem.move( stemsGroup, ElementPlacement.PLACEATEND);
+    }
+    originalStem.visible = false;
+//    It's impossible to fold layerSets
 }
