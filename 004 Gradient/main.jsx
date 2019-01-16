e39203a27c9b1573c139689ccb22bee8ab6b2e99
RedContritio <591369735@qq.com>
2019-01-16 12:02:03 +0800

    Update
    I think it is impossible to do what I want to do by it. If whoever know how to call lattice filter, I will be thankful and update this again.

--- 004 Gradient/main.jsx
+++ 004 Gradient/main.jsx
@@ -1,4 +1,14 @@
-﻿function color2desc( R, G, B)
+﻿function labels2List( labels, attr)
+{
+	var list = new ActionList();
+	for( var i = 0; i < labels.length; i++)
+	{
+		list.putObject( attr, labels[i]);
+	}
+	return list;
+}
+
+function RGB( R, G, B)
 {
 	var desc = new ActionDescriptor();
 	desc.putDouble( charIDToTypeID( "Rd  "), R);
@@ -18,72 +28,107 @@ function colorLabel2desc( rgbdesc, pos)
 }
 function colorLables2List( labels)
 {
-	var list = new ActionList();
-	for( var i = 0; i < labels.length; i++)
-	{
-		list.putObject( charIDToTypeID( "Clrt"), labels[i]);
-	}
-	return list;
+	return labels2List( labels, charIDToTypeID( "Clrt"));
 }
 
 function transparent2desc( pos, alpha)
 {
 	var desc = new ActionDescriptor();
-	desc.putUnitDouble( charIDToTypeID( "Opct"), charIDToTypeID( "#Prc"), alpha*100);
+	desc.putUnitDouble( charIDToTypeID( "Opct"), charIDToTypeID( "#Prc"), alpha);
 	desc.putInteger( charIDToTypeID( "Lctn"), pos*4096);
 	desc.putInteger( charIDToTypeID( "Mdpn"), 50 );
 	return desc;
 }
 function transparentLables2List( labels)
 {
-	var list = new ActionList();
-	for( var i = 0; i < labels.length; i++)
-	{
-		list.putObject( charIDToTypeID( "Trns"), labels[i]);
-	}
-	return list;
+	return labels2List( labels, charIDToTypeID( "Trns"));
 }
-var idMk = charIDToTypeID( "Mk  " );
-    var desc146 = new ActionDescriptor();
-    var idnull = charIDToTypeID( "null" );
-        var ref27 = new ActionReference();
-        var idGrdn = charIDToTypeID( "Grdn" );
-        ref27.putClass( idGrdn );
-    desc146.putReference( idnull, ref27 );
-    var idUsng = charIDToTypeID( "Usng" );
-        var desc147 = new ActionDescriptor();
-        var idGrad = charIDToTypeID( "Grad" );
-            var desc148 = new ActionDescriptor();
-            var idNm = charIDToTypeID( "Nm  " );
-            desc148.putString( idNm, """渐变名称""" );
-            var idGrdF = charIDToTypeID( "GrdF" );
-            var idGrdF = charIDToTypeID( "GrdF" );
-            var idCstS = charIDToTypeID( "CstS" );
-            desc148.putEnumerated( idGrdF, idGrdF, idCstS );
-            var idIntr = charIDToTypeID( "Intr" );
-            desc148.putDouble( idIntr, 4096.000000 );
-            var idClrs = charIDToTypeID( "Clrs" );
-					
-                    var desc150 = color2desc( 255.000000, 0.000000, 0.000000);
-                    var desc149 = colorLabel2desc( desc150, 0);
-                    var desc152 = color2desc( 0.000000, 0.000000, 120.000000 );
-				  var desc151 = colorLabel2desc ( desc152, 1);
-				  var colorlabels = new Array();
-				  colorlabels[0] = desc149;
-				  colorlabels[1] = desc151;
-					
-                var list22 = colorLables2List( colorlabels);
-            desc148.putList( idClrs, list22 );
-			
-			var idTrns = charIDToTypeID( "Trns" );
-			var desc153 = transparent2desc( 0, 1);
-			var desc154 = transparent2desc( 1, 1);
-			var transparentlabels = new Array( desc153, desc154);
-			var list23 = transparentLables2List( transparentlabels);
-			
-            desc148.putList( idTrns, list23 );
-        var idGrdn = charIDToTypeID( "Grdn" );
-        desc147.putObject( idGrad, idGrdn, desc148 );
-    var idGrdn = charIDToTypeID( "Grdn" );
-    desc146.putObject( idUsng, idGrdn, desc147 );
-executeAction( idMk, desc146, DialogModes.NO );
+
+function lists2Gradient( name, ColorList, TrnsList)
+{
+	var desc= new ActionDescriptor();
+	desc.putString( charIDToTypeID( "Nm  "), name);
+	desc.putEnumerated( charIDToTypeID( "GrdF"), charIDToTypeID( "GrdF"), charIDToTypeID( "CstS"));
+	desc.putDouble( charIDToTypeID( "Intr"), 4096.000000 ); // 4096 is Length of the scroll bar
+
+	desc.putList( charIDToTypeID( "Clrs"), ColorList);
+	desc.putList( charIDToTypeID( "Trns"), TrnsList);
+
+	return desc;
+}
+
+function Coord( x, y)
+{
+	var desc = new ActionDescriptor();
+	desc.putUnitDouble( charIDToTypeID( "Hrzn"), charIDToTypeID( "#Rlt"), x);
+	desc.putUnitDouble( charIDToTypeID( "Vrtc"), charIDToTypeID( "#Rlt"), y);
+	return desc;
+}
+
+function DeleteGradient( name)
+{
+	var idDlt = charIDToTypeID( "Dlt " );
+	var desc58 = new ActionDescriptor();
+	var idnull = charIDToTypeID( "null" );
+	var ref30 = new ActionReference();
+	var idGrdn = charIDToTypeID( "Grdn" );
+	ref30.putIndex( idGrdn, 20 ); // id : todo, change the value
+	desc58.putReference( idnull, ref30 );
+	executeAction( idDlt, desc58, DialogModes.NO );
+}
+function SaveGradient( GradientEntity)
+{
+		var GradAdd = new ActionDescriptor();
+		GradAdd.putObject( charIDToTypeID( "Grad" ), charIDToTypeID( "Grdn" ), GradientEntity );
+		
+		var MakeAction = new ActionDescriptor();
+		
+		var GradRef = new ActionReference();
+		GradRef.putClass( charIDToTypeID( "Grdn"));
+		MakeAction.putReference( charIDToTypeID( "null"), GradRef);
+		
+		MakeAction.putObject( charIDToTypeID( "Usng"), charIDToTypeID( "Grad"), GradAdd);
+		executeAction( charIDToTypeID( "Mk  " ), MakeAction, DialogModes.NO );
+}
+
+function ApplyGradient( GradientEntity, from, to)
+{
+    var GradAction = new ActionDescriptor();
+	
+	GradAction.putObject( charIDToTypeID( "From"), charIDToTypeID( "Pnt "), from);
+    GradAction.putObject( charIDToTypeID( "T   "), charIDToTypeID( "Pnt "), to);
+	
+    GradAction.putEnumerated( charIDToTypeID( "Type"), charIDToTypeID( "GrdT"),
+										charIDToTypeID( "Lnr "));
+    GradAction.putBoolean( charIDToTypeID( "Dthr"), true );
+    GradAction.putBoolean( charIDToTypeID( "UsMs"), true );
+	
+    GradAction.putObject( charIDToTypeID( "Grad"), charIDToTypeID( "Grdn"), GradientEntity);
+	executeAction( charIDToTypeID( "Grdn"), GradAction, DialogModes.NO );
+}
+
+function RandRange( low, high)
+{
+	return Math.floor(Math.random()*(high-low+1))+low;
+}
+
+var colorRed = RGB( 120.000000, 80.000000, 0.000000);
+var clrTag1 = colorLabel2desc( colorRed, 0);
+var colorDeepBlue = RGB( 0.000000, 80.000000, 80.000000 );
+var clrTag2 = colorLabel2desc ( colorDeepBlue, 1);
+var colorlabels = new Array( clrTag1, clrTag2);
+
+var TrnsTag1 = transparent2desc( 0, 100);
+var TrnsTag2 = transparent2desc( 0.5, 95);
+var TrnsTag3 = transparent2desc( 1, 80);
+var transparentlabels = new Array( TrnsTag1, TrnsTag2, TrnsTag3);
+
+var colorList = colorLables2List( colorlabels);
+var trnsList = transparentLables2List( transparentlabels);
+
+var MyGrad = lists2Gradient ( """name""", colorList, trnsList);
+//SaveGradient( MyGrad);
+
+//DeleteGradient();
+
+ApplyGradient( MyGrad, Coord(0,0), Coord(500,500));
